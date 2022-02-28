41eb7a571b541e7e68847d10ff133e91300737ca
Admin <Admin@DESKTOP-9833F0D>
2022-02-27 20:54:51 -0800

    1st comit

---
+++ asdf/src/zxcv/qwer.java
@@ -0,0 +1,15 @@
+
+package zxcv;
+
+public class qwer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=10;
+	int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
